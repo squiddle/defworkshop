a5ceba73af77c6f3952194eaef44d25d005b3059
Christian Schröder <christian.schroeder@conceptboard.com>
2013-10-12 15:14:49 +0200

    sequences nearly done and recursion examples

--- src/defworkshop/state_to_recursion.clj
+++ src/defworkshop/state_to_recursion.clj
@@ -3,60 +3,81 @@
 
 ;; More often than not, state can be avoided by using recursion. Let's see how it works
 
-(defn ^:not-implemented reverse-coll
+(defn reverse-coll
   "Reverse the collection `coll`.
 
    You can use `loop/recur` construct to loop over the sequence.
    `cons` prepends items to the list, try that out."
   [coll]
-  (…))
+  (loop [[head & tail] coll
+         acc '()]
+    (if (nil? head)
+      acc
+      (recur tail (cons head acc)))))
 
-(defn ^:not-implemented recursive-sum
+(defn recursive-sum
   "We've already implemented sum using reduce, now let's move to implementing it via recursion!"
   [[head & tail]]
-  (…))
+  (if (nil? head)
+    0
+    (+ head (recursive-sum tail))))
 
-(defn ^:not-implemented recursive-sum-tc
+(defn recursive-sum-tc
   "with a tail-recursive version of sum, we can avoid stack overflows."
   ([coll]
      (recursive-sum-tc coll 0))
   ([[head & tail] acc]
-     (…)))
+     (if (nil? head)
+      acc
+      (recursive-sum-tc tail (+ head acc)))))
 
-(defn ^:not-implemented max-from-list
+(defn max-from-list
   "Get the maximum from list using recursion"
   [[head & tail]]
   (if (empty? tail)
     head
-    (…)))
+    (max head (max-from-list tail))))
 
-(defn ^:not-implemented my-reduce
+(defn my-reduce
   "generalizing the recursive sum example, write your own implementation of reduce! (for empty coll, just return nil.)"
   ([f [head & tail]]
      (my-reduce f head tail))
   ([f acc-init coll]
-     (…)))
+     (if (empty? coll)
+      acc-init
+      (my-reduce f (f acc-init (first coll)) (rest coll)))))
 
-(defn ^:not-implemented max-from-list-tc
+(defn max-from-list-tc
   "Get the maximum from list using tail recursion (avoid stack overflow)"
   ([coll]
      (max-from-list-tc coll 0))
   ([[head & tail] max]
-     (…)))
+     (if (nil? head)
+      max
+      (max-from-list-tc tail (if (> head max) head max)))))
 
-(defn ^:not-implemented loop-recur-sum
+(defn loop-recur-sum
   "This implementation is somewhat easier to understand for people coming from imperative style."
   [numbers]
-  (…))
+  (loop [acc            0
+         [head & tail]  numbers]
+    (if (nil? head)
+      acc
+      (recur (+ acc head) tail))))
 
-(defn ^:not-implemented map-from-keys-and-vals
+(defn map-from-keys-and-vals
   "Something that we've already implemented previously in terms of `zipmap`, but are going to implement again
    in terms of recursion. Usually you use `loop/recur` construct whenever you have a one or multiple accumulators
    or several collections you iterate over."
   [keys vals]
-  (…))
+  (loop [acc {}
+         [key & key-tail] keys
+         [value & value-tail] vals]
+    (if (or (nil? key) (nil? value))
+      acc
+      (recur (assoc acc key value) key-tail value-tail))))
 
-(defn ^:not-implemented parentheses-balanced?
+(defn parentheses-balanced?
   "Check wether the given string has balanced parantheses or no.
 
    You can use `cond` statement to avoid deeply nested ints.
@@ -65,4 +86,9 @@
    `inc` increments a number, `dec` decrements a number."
   ([str] (parentheses-balanced? str 0))
   ([[current & tail] count]
-     (…)))
+    (if (nil? current) 
+      (= count 0)
+      (condp = current
+        \( (parentheses-balanced? tail (inc count))
+        \) (parentheses-balanced? tail (dec count))
+        (parentheses-balanced? tail count)))))
--- src/defworkshop/working_with_sequences.clj
+++ src/defworkshop/working_with_sequences.clj
@@ -1,130 +1,140 @@
 (ns defworkshop.working-with-sequences
   (:require [workshoplib.tdd :refer […]]))
 
-(defn ^:not-implemented split-head-and-tail
+(defn split-head-and-tail
   "Split head an tail of sequence by using `first` and `next` functions"
   [list]
-  (…))
+  [(first list) (next list)])
 
-(defn ^:not-implemented get-nth
+(defn get-nth
   "Now let's write a function that retrieves nth element from the sequence. For that, you can use `nth`"
   [seq n]
-  (…))
+  (nth seq n))
 
-(defn ^:not-implemented conj-to-list
+(defn conj-to-list
   "Conj is an operator that adds element to the list. In case with lists, it
    appends items in a reverse order."
   [list item]
-  (…))
+  (conj list item))
 
-(defn ^:not-implemented conj-to-vector
+(defn conj-to-vector
   "Conj operator with vector appends to tail of vector"
   [vector item]
-  (…))
+  (conj vector item))
 
-(defn ^:not-implemented remove-from-set
+(defn remove-from-set
   "Remove an element from a set using `disj`"
   [s to-remove]
-  (…))
+  (disj s to-remove))
 
-(defn ^:not-implemented rev-concat
+(defn rev-concat
   "given two sequences `s1` and `s2`, concatenate them such that the resulting sequence starts with the last element of `s2` and ends with the first element of `s1`. Hint: there's a function called `reverse` that might be of help."
   [s1 s2]
-  (…))
+  (concat (reverse s2) (reverse s1)))
 
-(defn ^:not-implemented interleave-seqs
+(defn interleave-seqs
   "given two sequences `s1` and `s2`, interleave them, starting with the shorter sequence."
   [s1 s2]
-  (…))
+  (if (<= (count s1) (count s2))
+    (interleave s1 s2)
+    (interleave s2 s1)))
 
-(defn ^:not-implemented split-head-and-tail-destructuring
+(defn split-head-and-tail-destructuring
   "Split head and tail of sequence by destructuring the input. Here, instead of `list` (see split-head-and-tail above), use [[head & tail]] which will assign the first element to `head` and
    the rest of sequence (denoted by &) to the tail."
   [list]
-  (…))
+  (let [[head & tail] list]
+    [head tail]))
 
-(defn ^:not-implemented contains-index
+(defn contains-index
   "`contains` is a somewhat confusing function. Usually people expect (contains? [1 2 3] 3) to return
    true, but it returns false, since it checks wether the sequence contains element with `key` (index, in
    case with vectors."
   [coll index]
-  (…))
+  (contains? coll index))
 
-(defn ^:not-implemented real-contains
+(defn real-contains
   "But if you use `some` instead of `contains` it will produce a desirable result. Let's implement a real
    contains with `some` and equality predicate"
   [coll item]
-  (…))
+  (some #(= item %) coll))
 
-(defn ^:not-implemented double-all
+(defn double-all
   "Double each item in sequence by using `map`."
   [l]
-  (…))
+  (map #(* 2 %) l))
 
-(defn ^:not-implemented add-5-to-all
+(defn add-5-to-all
   "Add 5 to each item in an array by using `map` and an anonymous function"
   [l]
-  (…))
+  (map #(+ 5 %) l))
 
-(defn ^:not-implemented add-5-to-all-with-for
+(defn add-5-to-all-with-for
   [l]
-  (…))
+  (for [x l] (+ 5 x)))
 
-(defn ^:not-implemented print-combinations
+(defn print-combinations
   "given two sequences `s1` and `s2`, print each combination of their elements where the elements aren't equal as a vector."
   [s1 s2]
-  (…))
+  (doseq [x1 s1 x2 s2 :when (not (= [x1] [x2]))] (println [x1 x2])))
 
-(defn ^:not-implemented split-to-pairs
+(defn split-to-pairs
   "Given a sequence, split it to pairs.
 
    You can use `partition` function for that."
   [seq]
-  (…))
+  (partition 2 seq))
 
-(defn ^:not-implemented moving-average
+(defn moving-average
   "given a sequence of numbers, calculate the moving average using `partition`."
   [nums window-size]
-  (…))
+  (->>
+    (partition window-size 1 nums)
+    (map #(apply + %))
+    (map #(/ % window-size))))
 
-(defn ^:not-implemented sum
+(defn sum
   "Calculate a sum of numbers in a list by using `reduce`"
   [list]
-  (…))
+  (reduce #(+ %1 %2) 0 list))
 
-(defn ^:not-implemented even-only
+(defn even-only
   "Filter even items from the list by using `filter` and `even?`"
   [l]
-  (…))
+  (filter even? l))
 
-(defn ^:not-implemented odd-only
+(defn odd-only
   "Filter out all the even items from the list, using `remove` (opposite of `filter`) and
    `even?`"
   [l]
-  (…))
+  (filter odd? l))
 
-(defn ^:not-implemented sum-of-squares-of-even-numbers
+(defn sum-of-squares-of-even-numbers
   "This excercise will help you to understand threading operator in Clojure"
   [coll]
-  (…))
+  (->>
+    coll
+    (filter even?)
+    (map #(* % %))
+    (reduce + 0)))
 
-(defn ^:not-implemented get-part-of-vector
+(defn get-part-of-vector
   "In order to get part of the vector, you can use `subvec`."
   ([vec start]
-     (…))
+     (subvec vec start))
   ([vec start end]
-     (…)))
+     (subvec vec start end)))
 
-(defn ^:not-implemented sort-sequence
+(defn sort-sequence
   "You often need to have a sequence sorted, for that you can use `sort` function"
   [seq]
-  (…))
+  (sort seq))
 
-(defn ^:not-implemented sort-maps
+(defn sort-maps
   "If you have a sequence of maps that has a structure [{:name \"Alex\" :age 25} {:name \"Maxi\" :age 26}],
    you can use `sort-by` to sort these maps by :age key"
   [seq]
-  (…))
+  (sort-by :age seq))
 
 (defn ^:not-implemented interpolate-words
   "Given a string of format \"Hello %name%, you're %age% years old!\", and map {:name \"Alex\" :age 25},
@@ -132,35 +142,39 @@
   [string m]
   (…))
 
-(defn ^:not-implemented repeat-times
+(defn repeat-times
   "For the given `number`, return a sequence where this number is repeated `n` times"
   [number n]
-  (…))
+  (repeat n number))
 
-(defn ^:not-implemented read-and-summarize-contents
+(defn read-and-summarize-contents
   "Given a filename of the file that holds numbers (newline-separated), write a function that parses numbers in file,
    summarizes them and returns. You can use `read-string` to parse the particular number."
   [filename]
-  (…))
+  (->>
+    (slurp filename)
+    (clojure.string/split-lines)
+    (map read-string)
+    (reduce +)))
 
-(defn ^:not-implemented reduce-assoc
+(defn reduce-assoc
   "In this function, use reduce to get a hash map from vector of pairs.
 
    This example illustrates how to use destructuring"
   [list]
-  (…))
+  (reduce #(let [[k v] %2] (assoc %1 k v)) {} (partition 2 list)))
 
-(defn ^:not-implemented stringify-map
+(defn stringify-map
   "Given an array of name and age, return a string that contains name and age in format `<name>: <age>`
 
    For that, use destructuring for keys and values and `str` to compose a new string."
   [m]
-  (…))
+  (vec (mapcat (fn [[name age]] [(str name ": " age)]) m)))
 
-(defn ^:not-implemented select-keysp
+(defn select-keysp
   "given a map `m`, return a map containing only the key-value pairs that satisfy `pred`."
   [m pred]
-  (…))
+  (reduce (fn [coll [k v]] (if (pred k) (assoc coll k v) coll)) {} m))
 
 (defn ^:not-implemented invert-map+
   "given a map, invert the mapping as previously. this time, however we will deal with duplicates in a defined way: for duplicate values in the original map, the inverted map should contain a mapping from value to a set of keys."
